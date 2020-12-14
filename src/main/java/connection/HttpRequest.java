016fbd487b38be37568d6948e9cf72abe17422b2
Senemedar <mr.cheesewick@gmail.com>
2020-12-14 19:41:29 +0100

    [feature] communication with ISS API now returns a JSON formatted String

---
+++ src/main/java/connection/HttpRequest.java
@@ -0,0 +1,55 @@
+package connection;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HttpRequest {
+	private final OkHttpClient client = HttpClient.getClient();
+	
+	private String run(String url) {
+		String httpResponse = null;
+		Request request = new Request.Builder()
+				.url(url)
+				.build();
+	
+		try {
+			Response response = client.newCall(request).execute();
+			if (response.body() != null) {
+				httpResponse = response.body().string();
+			} else {
+				// todo: ask if return string or maybe throw an exception?
+				httpResponse = "Invalid or no response";
+			}
+		} catch (IOException ignored) {}
+		
+		return httpResponse;
+	}
+	
+	public String runRequest(RequestType requestType, double... args) {
+		String runString;
+		switch (requestType) {
+			case CURRENT_LOCATION -> runString = ("http://api.open-notify.org/iss-now.json");
+			
+			case PEOPLE_IN_SPACE -> runString = ("http://api.open-notify.org/astros.json");
+			
+			case PASS_TIMES -> {
+				if (args.length == 3) {
+					runString = "http://api.open-notify.org/iss-pass.json?lat=" +
+							args[0] + "&lon=" +
+							args[1] + "&n=" +
+							args[2];
+				} else {
+					return "Zła liczna argumentów";
+				}
+			}
+			
+			default -> {
+				return "Niepoprawne zapytanie";
+			}
+			
+		}
+		return run(runString);
+	}
+}
