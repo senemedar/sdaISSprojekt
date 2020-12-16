ce15ca0e875cbbe364bbbe71b67aa65e7f30c9ab
Senemedar <mr.cheesewick@gmail.com>
2020-12-16 21:12:47 +0100

    [update] method calling API has a new name; exceptions added (+tests)

--- src/main/java/functionality/functions/Main.java
+++ src/main/java/functionality/functions/Main.java
@@ -1,18 +1,25 @@
 package functionality.functions;
 
+import connection.IssApiCall;
 import connection.RequestType;
-import connection.HttpRequest;
+import connection.WrongNumberOfArgumentsException;
 
 
 public class Main {
 	public static void main(String[] args) {
-		HttpRequest httpRequest = new HttpRequest();
+		IssApiCall issApiCall = new IssApiCall();
 //		String response = connection.run("https://raw.github.com/square/okhttp/master/README.md");
-		String response1 = httpRequest.runRequest(RequestType.CURRENT_LOCATION);
-		String response2 = httpRequest.runRequest(RequestType.PEOPLE_IN_SPACE);
-		String response3 = httpRequest.runRequest(RequestType.PASS_TIMES, 0.1, 0.1, 2);
-		System.out.println(response1);
+//		String response1 = issApiCall.runRequest(RequestType.CURRENT_LOCATION);
+		String response2 = null;
+		try {
+			response2 = issApiCall.runRequest(RequestType.PASS_TIMES, 0.1);
+		} catch (WrongNumberOfArgumentsException e) {
+			e.printStackTrace();
+		}
+//		String response3 = issApiCall.runRequest(RequestType.PASS_TIMES, 0.1, 0.1, 2);
+//		System.out.println(response1);
 		System.out.println(response2);
-		System.out.println(response3);
+//		System.out.println(response3);
+		
 	}
 }
