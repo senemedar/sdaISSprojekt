eb76c54a40155183b8689ab4aa984b6376e59070
Patryk <pabd9009@wp.pl>
2021-01-02 15:10:23 +0100

    Gitignore fix, add connection package files to vcs
    For reasons unknown, connection files appeared unversioned after checkout to remote origin/master

---
+++ src/main/java/ISS/connection/RequestType.java
@@ -0,0 +1,7 @@
+package ISS.connection;
+
+public enum RequestType {
+	CURRENT_LOCATION,
+	PASS_TIMES,
+	PEOPLE_IN_SPACE
+}
---
+++ src/main/java/ISS/functionality/functions/Main.java
@@ -0,0 +1,29 @@
+package ISS.functionality.functions;
+
+import ISS.connection.IssApiCall;
+import ISS.connection.RequestType;
+import ISS.connection.WrongNumberOfArgumentsException;
+
+
+public class Main {
+	public static void main(String[] args) {
+/*		IssApiCall issApiCall = new IssApiCall();
+//		String response = ISS.connection.run("https://raw.github.com/square/okhttp/master/README.md");
+//		String response1 = issApiCall.runRequest(RequestType.CURRENT_LOCATION);
+		String response2 = null;
+		try {
+			response2 = issApiCall.runRequest(RequestType.PASS_TIMES, 0.1);
+		} catch (WrongNumberOfArgumentsException e) {
+			e.printStackTrace();
+		}
+//		String response3 = issApiCall.runRequest(RequestType.PASS_TIMES, 0.1, 0.1, 2);
+//		System.out.println(response1);
+		System.out.println(response2);
+//		System.out.println(response3);*/
+
+
+		System.out.println("Hello world");
+
+
+	}
+}
